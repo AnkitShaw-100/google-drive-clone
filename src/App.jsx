d530c3c2789c2160d4ec93758644a7a3e34c63e3
AnkitShaw_100 <ankitshaw6933@gmail.com>
2025-03-02 19:37:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Body from "./components/Body";
+import Inbox from "./components/Inbox";
+import Mail from "./components/Mail";
+import "./App.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/inbox",
+        element: <Inbox />,
+      },
+      {
+        path: "/mail/:id",
+        element: <Mail />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <div className="bg-[#F6f8FC] h-screen overflow-hidden">
+      <Navbar />
+      <RouterProvider router={router} />
+      {/* <Sidebar /> */}
+      {/* <h1 class="text-6xl font-bold underline">Hello world!</h1> */}
+    </div>
+  );
+}
+
+export default App;
